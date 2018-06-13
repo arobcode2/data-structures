fc96f2303593f55cb775becf14af92972ad3475a
arobcode2 <andrew-c-rob@live.com>
2018-06-13 15:32:13 -0500

    Functional stack werks!

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,22 @@
 var Stack = function() {
   var someInstance = {};
-
+  someInstance.counter = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[this.counter] = value;
+    this.counter ++;
   };
 
   someInstance.pop = function() {
+    this.counter --;
+    return storage[this.counter];
   };
 
   someInstance.size = function() {
+    return this.counter;
   };
 
   return someInstance;
