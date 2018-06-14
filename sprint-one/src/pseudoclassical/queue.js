107ca360b8ca679f479316f6747552c24c73773c
arobcode2 <andrew-c-rob@live.com>
2018-06-13 19:49:45 -0500

    Implement queue for the pseudoclassical pattern

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.pointer = 0;
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.pointer + this.length] = value;
+  this.length ++;
+};
 
+Queue.prototype.dequeue = function() {
+  if (this.length > 0) {
+    this.length --;
+    this.pointer ++;
+    return this.storage[this.pointer - 1];
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.length;
+};
