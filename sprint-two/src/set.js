3355e1e690b621208745ff84e6bcfb9fb1f3eb1d
arobcode2 <andrew-c-rob@live.com>
2018-06-14 15:56:32 -0500

    Implemented Set for sprint two in data structures

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,39 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; // fix me
+  set.size = 0;
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this.contains(item)) return;
+    this._storage.push(item);
+    this.size++;
 };
 
 setPrototype.contains = function(item) {
+  var bool = false;
+  
+  for (var i = 0; i <= this.size; i++) {
+    if (this._storage[i] === item) {
+      bool = true
+      return bool;
+    }
+  }
+  return bool;
 };
 
 setPrototype.remove = function(item) {
+  if (this.size > 0) {
+    for (var i = 0; i < this.size; i++) {
+      if (this._storage[i] === item) {
+        this._storage.splice(i, 1);
+        this.size--;   
+      }
+    }
+  }
 };
 
 /*
