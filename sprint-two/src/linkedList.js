0ef3142667c7f10028af7e181950dada10acbcfa
arobcode2 <andrew-c-rob@live.com>
2018-06-14 10:45:05 -0500

    Completed Linked List instantiation (data structures sprint 2)

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,35 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    if (!list.head) {
+      var someNode = Node(value);
+      list.head = someNode;
+      list.tail = someNode;
+    } else {
+      var someNode = Node(value);
+      list.tail.next = someNode;
+      list.tail = someNode;
+    }
   };
 
   list.removeHead = function() {
+    var output = list.head;
+    list.head = output.next
+    return output.value;  
   };
 
   list.contains = function(target) {
+    if (list.head) {
+      var someNode = list.head
+      while (list.tail !== someNode){
+        if (someNode.value === target){
+          return true 
+        } else {
+          someNode = someNode.next 
+        }
+      } 
+        return (list.tail.value === target);
+    }
   };
 
   return list;
