369351c040844470a3bfb191173df27bea65e0e5
arobcode2 <andrew-c-rob@live.com>
2018-06-14 12:33:22 -0500

    We did it! Oh sweet dino baby! we did it! I wanted to throw the computer, but I didn't and we did it. ( we completed the tree thing-a-ma-jig)

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,35 @@
 var Tree = function(value) {
   var newTree = {};
+  _.extend(newTree,treeMethods);
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; 
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var myChildTree = Tree(value);
+  this.children.push(myChildTree);
 };
 
 treeMethods.contains = function(target) {
+  var doesContain = false
+  var hasTarget = function (tree) {
+    if (tree.value === target){
+      doesContain = true
+      return doesContain
+    } else {
+    //pop down to a each child
+    //check if child has the value we want if it does, set Does containt to true, and return it//
+    //else pop down ...
+    for (var i = tree.children.length - 1; i >= 0; i--) {
+      hasTarget(tree.children[i])
+    }
+    }
+  }
+  hasTarget(this)
+  return doesContain;
 };
 
 
